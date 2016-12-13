eaa8a4cb263a5a3e8dd3486974571b540d1bda02
rahulchaudhary01 <er.chaudhary.rahul@gmail.com>
2016-12-13 21:51:21 +0530

    Initial commit

---
+++ app/src/main/java/com/android/appstry/mypaytm/PaytmMain.java
@@ -0,0 +1,19 @@
+package com.android.appstry.mypaytm;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class PaytmMain extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paytm_main);
+    }
+}
